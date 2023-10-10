b2b3754872b75620298b1ace07ed6337bcd8e76b
Dharane-Segar <dharanesegar08@gmail.com>
2023-10-10 16:25:45 +0530

    Changed environment variables

--- src/test/java/com/example/britishAirwaysCheckIn/britishAirwaysCheckIn/BritishAirwaysCheckInApplicationTests.java
+++ src/test/java/com/example/britishAirwaysCheckIn/britishAirwaysCheckIn/BritishAirwaysCheckInApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BritishAirwaysCheckInApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+////	void contextLoads() {
+////	}
 
 }
