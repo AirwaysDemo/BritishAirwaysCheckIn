7141ddfdd9b59345f83ccf34bd971f3414e36287
IT21068478 <dharanesegar08@gmail.com>
2023-10-28 20:21:42 +0530

    Test

--- src/main/java/com/example/britishAirwaysCheckIn/britishAirwaysCheckIn/controller/CheckInController.java
+++ src/main/java/com/example/britishAirwaysCheckIn/britishAirwaysCheckIn/controller/CheckInController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/britishairways/checkin")
 public class CheckInController {
+    
 
     @Autowired
     CheckInService checkinService;
